3c56852f3e0f07f493c7f5a33c9f3b7cc76478e6
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2022-04-29 19:39:47 -0500

    Update PAA.py

--- PAA.py
+++ PAA.py
@@ -9,6 +9,7 @@ st.text ("Enter a Query and See What People Also Asked on Google")
 query = st.text_input("Write Your Question")
 
 results = people_also_ask.get_related_questions(query)
+results = results.split("?", 1)
 
 st.write(results)
 
