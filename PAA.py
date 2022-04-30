a8382d87c0abafed9f4d66016bc1a3cf4c93f922
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2022-04-29 19:50:17 -0500

    Update PAA.py

--- PAA.py
+++ PAA.py
@@ -11,7 +11,7 @@ query = st.text_input("Write Your Query")
 #results = people_also_ask.get_related_questions(query)
 
 answer = people_also_ask.get_answer(query)
-related = answer.get("related_questions"))
+related = answer.get("related_questions")
 
 st.write(related)
 
