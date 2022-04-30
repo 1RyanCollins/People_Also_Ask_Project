7316ede905e4b992fabb7f08b62ed84844f995f1
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2022-04-29 19:14:39 -0500

    Update PAA.py

--- PAA.py
+++ PAA.py
@@ -2,6 +2,10 @@ import people_also_ask
 import streamlit as st
 
 
-people_also_ask.get_related_questions("coffee")
+result = people_also_ask.get_related_questions("coffee")
+
+st.write(result)
+
+
 
 
