f22d94766250c40f79eb8a5d9d4caf43ac00cb0c
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2022-04-29 19:37:48 -0500

    Update PAA.py

--- PAA.py
+++ PAA.py
@@ -6,9 +6,13 @@ import streamlit as st
 st.title("People Also Ask Project")
 st.text ("Enter a Query and See What People Also Asked on Google")
 
-query = st.text_input("Write Your Query")
+query = st.text_input("Write Your Question")
 
-for question in people_also_ask.generate_related_questions("cofee")
+results = people_also_ask.get_related_questions(query)
+
+st.write(results)
+
+
 
 
 
