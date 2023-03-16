add4c11071af44544f1e487c685f8c57f028e411
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2023-03-16 14:19:49 -0500

    Update PAA.py

--- PAA.py
+++ PAA.py
@@ -6,7 +6,9 @@ title("People Also Ask Link Analysis Project")
 
 question = st.text_input("Write Your Question")
 
-answer = people_also_ask.get_answer(question)
+x = people_also_ask.get_answer(question)
+
+st.write(x)
 
 
 
