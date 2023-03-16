5d4394e49d488e72b85d8d8bfba0c638ae3862ad
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2023-03-16 13:53:12 -0500

    Update PAA.py

--- PAA.py
+++ PAA.py
@@ -9,11 +9,9 @@ question = st.text_input("Write Your Question")
 
 result = people_also_ask.get_answer(question)
 
+result["displayed_link"]
 
 
-st.write(result["displayed_link"])
-
-st.text(result["displayed_link"])
 
 
 
