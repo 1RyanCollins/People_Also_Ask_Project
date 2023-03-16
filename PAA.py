4cbb926cea160be8e65bc35e6f1044ac4efdda63
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2023-03-16 13:45:34 -0500

    Update PAA.py

--- PAA.py
+++ PAA.py
@@ -2,18 +2,14 @@ import people_also_ask
 import streamlit as st
 
 
-
-st.title("People Also Ask Project")
+st.title("People Also Ask Link Analysis Project")
 st.text ("Enter a Query")
 
-query = st.text_input("Write Your Query")
-
-#results = people_also_ask.get_related_questions(query)
+question = st.text_input("Write Your Question")
 
 answer = people_also_ask.get_answer(query)
-related = answer.get("related_questions")
 
-st.write(related)
+st.write(answer)
 
 
 #st.write(results)
